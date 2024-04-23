536deab0cd4266b98663fd09c6a1e67903f92f34
robteach <robert.cleary.teach@gmail.com>
2024-04-23 18:29:08 +0100

    add SlideExamples02a.sql, _querypracitce02.sql, updated links.txt and live_demo_solutions.sql from previous session

---
+++ _querypractice02.sql
@@ -0,0 +1,38 @@
+-- 1.
+-- ## Go to:
+-- https://pgexercises.com/gettingstarted.html
+
+-- 2.
+-- Manually create the tables cd.facilities and cd.members 
+-- shown at the URL above (make cd.bookings too if you like)
+-- [NOTE: you can just put the tables inside
+--        the default `postgres` database for now.]
+
+-- 2.a: For each table (facilities/members/bookings)
+--		Explain what can... and what can't... happen?
+--      {NOTE: ignore and references to 'FOREIGN KEY' for now]
+
+-- 2.b: Do a SELCT * on each and see NO Data yet.
+
+-- 2.c:
+	-- Download the sql file for that DB here:
+	-- https://pgexercises.com/dbfiles/clubdata.sql
+	
+-- Once downloaded `clubdata.sql` 
+-- Scroll-down (line 3135)
+-- and use the INSERTs for facilities and members
+-- to populate those tables with some data
+
+-- 2.d:
+-- ## Go to:
+-- https://pgexercises.com/questions/basic/selectall.html
+
+-- and figure out what query to run
+--  test it on your local clubdata tables
+
+
+-- 3.
+-- ## Follow this tutorial:
+-- https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-date/
+
+
